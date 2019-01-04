a7bdb7690faec8cebd2d8e400fd5bbdd64e629df
ardhapanduwinata <ardhapanduwinata@gmail.com>
2019-01-04 14:01:38 +0700

    nambahin manage data

--- application/views/header&footer/admin/v_headerManageData.php
+++ application/views/header&footer/admin/v_headerManageData.php
@@ -9,22 +9,25 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <title><?= $title ?></title>
+
+    <link href="<?=base_url('assets')?>/img/favicon.ico" rel="shortcut icon">
+    <link href="<?=base_url('assets')?>/img/apple-icon-180x180.png" rel="apple-touch-icon">
 
     <!-- Bootstrap Core CSS -->
     <link href="<?= base_url('assets') ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="<?= base_url('assets') ?>//vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets') ?>/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="<?= base_url('assets') ?>//dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?= base_url('assets') ?>/dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="<?= base_url('assets') ?>//vendor/morrisjs/morris.css" rel="stylesheet">
+    <link href="<?= base_url('assets') ?>/vendor/morrisjs/morris.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="<?= base_url('assets') ?>//vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets') ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,3 +37,67 @@
     <![endif]-->
 
 </head>
+<body>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+        </div>
+        <!-- /.navbar-header -->
+
+        <ul class="nav navbar-top-links navbar-right">
+            <li>
+                <a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+            </li>
+            <li>
+                <a href="<?= base_url('homeAdmin')?>"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+            </li>
+        </ul>
+        <!-- /.navbar-top-links -->
+
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li class="sidebar-search">
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Search...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                        <!-- /input-group -->
+                    </li>
+                    <li>
+                        <a href="<?= base_url('manageData')?>"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('manageData/jurusan') ?>"><i class="fa fa-table fa-fw"></i> Jurusan</a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('manageData/Kamus')?>"><i class="fa fa-book fa-fw"></i> Kamus</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.sidebar-collapse -->
+        </div>
+        <!-- /.navbar-static-side -->
+    </nav>
+
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header"><?= $page_header ?></h1>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
