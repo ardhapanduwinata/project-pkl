225a9380cb5039105746ca1717bcbedf62dee7a5
rmillenia <rmillenia96@yahoo.com>
2019-01-07 14:40:03 +0700

    update biodata user

--- application/controllers/user/homeUser.php
+++ application/controllers/user/homeUser.php
@@ -34,10 +34,152 @@ class homeUser extends CI_Controller {
     {
         $data['title'] = "Manage Biodata";
         $data['siapa'] = $this->session->userdata('nama');
+        $data['id']    = $this->session->userdata('id');
+        $data['jurusan'] = $this->models->get_data('jurusan')->result();
+
+        $table  = 'users';
+        $table1 = 'mhs';
+
+        $where = array(
+            'users.id_user' => $data['id']
+        );
+
+        $on = 'mhs.id_user = users.id_user';
+        $data['user'] = $this->models->get_selected_join($table,$table1,$where,$on)->result();
 
         $this->load->view('header&footer/user/v_headerUser', $data);
-        $this->load->view('user/v_biodataUser');
+        $this->load->view('user/v_biodataUser',$data);
         $this->load->view('header&footer/user/v_footerUser');
         $this->load->view('v_modals');
+    }
+
+    public function biodataPass()
+    {
+        $data['title'] = "Manage Biodata";
+        $data['siapa'] = $this->session->userdata('nama');
+        $data['id']    = $this->session->userdata('id');
+
+        $table  = 'users';
+        $table1 = 'mhs';
+
+        $where = array(
+            'users.id_user' => $data['id']
+        );
+
+        $on = 'mhs.id_user = users.id_user';
+        $data['user'] = $this->models->get_selected_join($table,$table1,$where,$on)->result();
+
+
+        $this->load->view('header&footer/user/v_headerUser', $data);
+        $this->load->view('user/v_biodataPassUser',$data);
+        $this->load->view('header&footer/user/v_footerUser');
+        $this->load->view('v_modals');
+    }
+
+    public function updatePhoto()
+    {
+        $id = $this->session->userdata('id');
+
+        $where = array(
+            'id_user' => $id
+        );
+
+        $config['upload_path']='./assets/img/foto_users/';
+        $config['allowed_types']='jpg|png|jpeg';
+        $config['max_size']=1000000000;
+        $config['max_width']=10240;
+        $config['max_height']=7680;
+
+        $this->load->library('upload', $config);
+        if (! $this->upload->do_upload('pic')) {
+            $error = array('error' => $this->upload->display_errors());
+            redirect('user/homeUser/biodata','refresh');
+
+        }else{
+            $data = array(
+                'foto' => $this->upload->data('file_name')
+            );
+
+            $this->models->update_data('users',$data,$where);
+                echo "<script>alert('Foto user berhasil diubah'); </script>";
+                redirect('user/homeUser/biodata','refresh');
+        }
+    }
+
+    public function updatePass($idUpdate)
+    {
+        $id    = $this->session->userdata('id');
+
+        $where = array(
+            'id_user' => $id
+        );
+
+        if ($idUpdate==1){
+            $this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[users.username]');
+            if ($this->form_validation->run()==FALSE){
+                echo '<script>alert("'.str_replace(array("\r", "\n"), '\n', strip_tags(validation_errors())).'");</script>';
+                redirect('user/homeUser/biodataPass','refresh');
+            }else{
+            $data = array(
+                'username' => $this->input->post("username")
+            );
+
+            $this->models->update_data('users', $data, $where);
+            echo "<script>alert('Username berhasil diubah'); </script>";
+            redirect('user/homeUser/biodataPass', 'refresh');
+            }
+        }else{
+            $old = $this->input->post('old');
+            $new = $this->input->post('password');
+            $confirm = $this->input->post('confirm');
+
+            $data = array(
+                'password' => md5($this->input->post("password"))
+            );
+            if(md5($old) == $this->session->userdata('pass')){
+                if ($new == $confirm) {
+                    $this->models->update_data('users', $data, $where);
+                    echo "<script>alert('Password berhasil diubah'); </script>";
+                    redirect('user/homeUser/biodataPass', 'refresh');
+                    $this->session->set_userdata('pass',$new);
+                }else{
+                    echo "<script>alert('Password tidak sesuai'); </script>";
+                    redirect('user/homeUser/biodataPass', 'refresh');
+                }
+            }else{
+                echo "<script>alert('Password lama yang anda masukan salah'); </script>";
+                redirect('user/homeUser/biodataPass', 'refresh');
+            }
+
+        }
+    }
+
+    public function updateBio()
+    {
+        $id    = $this->session->userdata('id');
+
+        $data = array(
+            'nim' => $this->input->post("nim"),
+            'nama_mhs' => $this->input->post("nama"),
+            'id_jurusan' => $this->input->post("jurusan"),
+            'univ' => $this->input->post("univ"),
+            'alamat' => $this->input->post("alamat"),
+            'email' => $this->input->post("email")
+        );
+
+        $data1 = array(
+            'nama_user' => $this->input->post("nama")
+        );
+
+        $where = array(
+            'id_user' => $id
+        );
+
+        $this->models->update_data('mhs',$data,$where);
+        $this->models->update_data('users',$data1,$where);
+        $this->session->set_userdata('nama',$this->input->post("nama"));
+        echo "<script>alert('Profil User berhasil diubah'); </script>";
+        redirect('user/homeUser/biodata','refresh');
+
     }
 }
--- application/views/user/v_biodataUser.php
+++ application/views/user/v_biodataUser.php
@@ -1,30 +1,31 @@
-<div class="container" style="padding-top: 10px;">
+
+<div class="container" style="padding-top: 20px; padding-bottom: 20px">
 
     <div class="row">
         <div class="col-md-3">
 
             <div class="list-group " style="padding-bottom: 10px">
-                <a href="#" class="list-group-item list-group-item-action active">User Profile</a>
-                <a href="#" class="list-group-item list-group-item-action">User Management</a>
+                <a href="<?php echo base_url('user');?>/homeUser/biodata" class="list-group-item list-group-item-action active " style="background-color: #0c2e8a;border-color: #0c2e8a">User Profile</a>
+                <a href="<?php echo base_url('user');?>/homeUser/biodataPass" class="list-group-item list-group-item-action ">User Management</a>
 
             </div>
-
-            <div class="card" style="padding: 10px">
+            <br>
+            <div class="card" style="padding: 15px;">
                 <div class="text-center">
-                    <?php echo form_open_multipart('UserDetail/updatePhoto'); ?>
-                    <img src="<?php echo base_url('assets');?>/img/foto_users/default.png" class="avatar img-circle" alt="avatar" width="150" height="150">
+                    <?php echo form_open_multipart('user/homeUser/updatePhoto'); ?>
+                    <img src="<?php echo base_url();?>assets/img/foto_users/<?php echo $user[0]->foto?>" style="border: groove" class="avatar img-circle" alt="avatar" width="150" height="150">
+                    <br><br>
                     <h6>Upload a different photo...</h6>
-
                     <input type="file" class="form-control" name="pic">
                     <br>
-                    <input type="submit" name="submit" class="btn btn-info" value="Update">
+                    <input type="submit" name="submit" class="btn" value="Update">
                     <?php echo form_close();?>
                 </div>
             </div>
         </div>
 
         <div class="col-md-9">
-            <div class="card">
+            <div class="card" style="height: 483px">
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-12">
@@ -34,70 +35,56 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <form>
+                            <?php echo form_open_multipart('user/homeUser/updateBio'); ?>
+
                                 <div class="form-group row">
-                                    <label for="username" class="col-4 col-form-label">User Name*</label>
+                                    <label for="nim" class="col-4 col-form-label">NIM</label>
                                     <div class="col-8">
-                                        <input id="username" name="username" placeholder="Username" class="form-control here" required="required" type="text">
+                                        <input id="nim" name="nim" placeholder="NIM" class="form-control here" value="<?= $user[0]->nim?>" type="text" required="required">
+                                </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="nama" class="col-4 col-form-label">Nama Lengkap</label>
+                                    <div class="col-8">
+                                        <input id="nama" name="nama" placeholder="Nama Lengkap" class="form-control here" value="<?= $user[0]->nama_user?>" type="text" required="required">
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="email" class="col-4 col-form-label">Email*</label>
+                                    <label for="jurusan" class="col-4 col-form-label">Jurusan</label>
                                     <div class="col-8">
-                                        <input id="email" name="email" placeholder="Email" required="required" class="form-control here" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="name" class="col-4 col-form-label">First Name</label>
-                                    <div class="col-8">
-                                        <input id="name" name="name" placeholder="First Name" class="form-control here" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="lastname" class="col-4 col-form-label">Last Name</label>
-                                    <div class="col-8">
-                                        <input id="lastname" name="lastname" placeholder="Last Name" class="form-control here" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="website" class="col-4 col-form-label">Website</label>
-                                    <div class="col-8">
-                                        <input id="website" name="website" placeholder="Website" class="form-control here" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="pass" class="col-4 col-form-label">Password</label>
-                                    <div class="col-8">
-                                        <input id="pass" name="pass" placeholder="Password" class="form-control here" required="required" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-4">Send User Notification</label>
-                                    <div class="col-8">
-                                        <label class="custom-control custom-checkbox">
-                                            <input name="checkbox" class="custom-control-input" value="user-notification" type="checkbox">
-                                            <span class="custom-control-indicator"></span>
-                                            <span class="custom-control-description">Send the new user an email about their account.</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="role" class="col-4 col-form-label">Role</label>
-                                    <div class="col-8">
-                                        <select id="role" name="role" class="custom-select">
-                                            <option value="administrator">Administrator</option>
-                                            <option value="author">Author</option>
-                                            <option value="editor">Editor</option>
-                                            <option value="contributor">Contributor</option>
+                                        <select id="jurusan" name="jurusan" class="custom-select" required="required">
+                                            <?php foreach($jurusan as $row) {
+                                                $cat = $row->id_jurusan;
+                                                ?>
+                                                <option <?php if($cat ==  $user[0]->id_jurusan) echo"selected"; ?> value="<?php echo $row->id_jurusan;?>"><?php echo $row->jurusan;?></option>
+                                            <?php } ?>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <div class="offset-4 col-8">
-                                        <button name="submit" type="submit" class="btn btn-primary">Update Profile</button>
+                                    <label for="universitas" class="col-4 col-form-label">Universitas</label>
+                                    <div class="col-8">
+                                        <input id="univ" name="univ" placeholder="Universitas Anda" value="<?= $user[0]->univ?>" class="form-control here" type="text" required="required">
                                     </div>
                                 </div>
-                            </form>
+                                <div class="form-group row">
+                                    <label for="alamatUniv" class="col-4 col-form-label">Alamat Universitas</label>
+                                    <div class="col-8">
+                                        <input id="alamat" name="alamat" placeholder="Alamat Universitas Anda" value="<?= $user[0]->alamat?>" class="form-control here" type="text" required="required">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="email" class="col-4 col-form-label">Email</label>
+                                    <div class="col-8">
+                                        <input id="email" name="email" placeholder="Email" class="form-control here" value="<?= $user[0]->email?>" type="text" required="required">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-4 col-8">
+                                        <button name="submit" type="submit" class="btn">Update Profile</button>
+                                    </div>
+                                </div>
+                            <?php echo form_close();?>
                         </div>
                     </div>
 
