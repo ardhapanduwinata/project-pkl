df52d3f150c370a8b5d7dac45cbcd9616006fadc
ardhapanduwinata <ardhapanduwinata@gmail.com>
2019-01-22 08:16:46 +0700

    edit home page

--- application/controllers/admin/homeAdmin.php
+++ application/controllers/admin/homeAdmin.php
@@ -29,7 +29,7 @@ class homeAdmin extends CI_Controller {
 //        $data['intro'] = $this->models->get_data('home_page')->result();
 
         $this->load->view('header&footer/admin/v_headerHome', $data);
-        $this->load->view('admin/v_homeAdmin');
+        $this->load->view('admin/home/v_homeAdmin');
         $this->load->view('header&footer/admin/v_footerHome');
         $this->load->view('v_modals');
     }
@@ -60,5 +60,29 @@ class homeAdmin extends CI_Controller {
         }
         echo "<script>alert('Berhasil ditambahkan'); </script>";
         redirect(base_url('admin/homeAdmin'));
+    }
+
+    public function edit_intro()
+    {
+        $data['title'] = "Edit Intro";
+        $data['siapa'] = $this->session->userdata('nama');
+        $data['judul'] = "Gambar yang terupload";
+        $data['param'] = "intro";
+
+        $where = array('nama_konten' => "intro");
+        $data['intro'] = $this->models->get_selected('home_page', $where)->result();
+
+        $this->load->view('header&footer/admin/v_headerHome', $data);
+        $this->load->view('admin/home/v_edit_intro', $data);
+        $this->load->view('header&footer/admin/v_footerHome');
+        $this->load->view('v_modals');
+    }
+
+    public function del_intro($id)
+    {
+        $where = array('id_home' => $id);
+
+        $this->models->delete_data($where, 'home_page');
+        redirect(base_url('admin/homeAdmin/edit_intro'));
     }
 }
