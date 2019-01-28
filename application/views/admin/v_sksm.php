59d967690451bd5a9a39b5f1dc42ac83fae6bdaa
ardhapanduwinata <ardhapanduwinata@gmail.com>
2019-01-28 20:48:21 +0700

    g

---
+++ application/views/admin/v_sksm.php
@@ -0,0 +1,212 @@
+<html>
+<head>
+    <title><?= $title ?></title>
+    <link href="<?=base_url('assets')?>/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?=base_url('assets')?>/img/favicon.ico" rel="shortcut icon">
+    <link href="<?=base_url('assets')?>/img/apple-icon-180x180.png" rel="apple-touch-icon">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <script src="https://unpkg.com/docx@4.0.0/build/index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.js"></script>
+    <script src="<?= base_url('assets') ?>/FileServer.js/dist/FileServer.js"></script>
+    <script src="<?= base_url('assets') ?>/FileServer.js/src/FileServer.js"></script>
+</head>
+
+<script>
+    function Export2Doc(element, filename = ''){
+        var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+        var postHtml = "</body></html>";
+        var html = preHtml+document.getElementById(element).innerHTML+postHtml;
+
+        var blob = new Blob(['\ufeff', html], {
+            type: 'application/msword'
+        });
+
+        // Specify link url
+        var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+
+        // Specify file name
+        filename = filename?filename+'.doc':'document.doc';
+
+        // Create download link element
+        var downloadLink = document.createElement("a");
+
+        document.body.appendChild(downloadLink);
+
+        if(navigator.msSaveOrOpenBlob ){
+            navigator.msSaveOrOpenBlob(blob, filename);
+        }else{
+            // Create a link to the file
+            downloadLink.href = url;
+
+            // Setting the file name
+            downloadLink.download = filename;
+
+            //triggering the function
+            downloadLink.click();
+        }
+
+        document.body.removeChild(downloadLink);
+    }
+</script>
+<?php
+function tgl_indo($tanggal){
+    $bulan = array (
+        1 =>   'Januari',
+        'Februari',
+        'Maret',
+        'April',
+        'Mei',
+        'Juni',
+        'Juli',
+        'Agustus',
+        'September',
+        'Oktober',
+        'November',
+        'Desember'
+    );
+    $pecahkan = explode('-', $tanggal);
+
+    // variabel pecahkan 0 = tanggal
+    // variabel pecahkan 1 = bulan
+    // variabel pecahkan 2 = tahun
+
+    return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+}
+?>
+</html>
+
+<br><br>
+
+
+<?php foreach ($datamagang as $a)
+    $no = 1;
+$source = $a['tgl_mohon_surat'];
+{ ?>
+    <div style="padding-top: 20px; padding-right: 80px; float: right">
+        <a style="float: right; margin-right: 80px" href="<?= base_url('admin/manageData/permohonan/semua')?>"><button type="button" class="btn btn-danger">Kembali</button></a>
+        <button style="float: right; margin-right: 5px" onclick="Export2Doc('isi_data','SL-<?= date("Y") ?>.SuratKeteranganSelesaiMagang.<?= $a['nim']?>.<?= $a['nama_mhs']?>')" class="btn btn-primary">Download as Word</button>
+
+    </div>
+    <div id="isi_data" style="padding-top: 100px;">
+        <div style="padding-left: 80px">
+            <table width="100%">
+                <tr>
+                    <td style="width: 10%">Nomor</td>
+                    <td>:</td>
+                    <td>&emsp;&emsp;&emsp;&emsp;&emsp;/SDM.04.06/DIVTLN/<?= date('Y')?></td>
+                    <td style="text-align: right; padding-right: 40px"><?= tgl_indo(date('Y-m- ')) ?>&emsp;&emsp;&emsp;</td>
+                </tr>
+                <tr>
+                    <td>Lampiran</td>
+                    <td>:</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Sifat</td>
+                    <td>:</td>
+                    <td>Biasa</td>
+                </tr>
+                <tr>
+                    <td>Perihal</td>
+                    <td>:</td>
+                    <td><?= $perihal ?></td>
+                    <td>Kepada</td>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td>Yth. Rektor <?= $a['univ'] ?></td>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> <?= $a['alamat'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <br><br>
+
+        <div style="margin-left: 80px; margin-right: 80px; padding-left: 160px; padding-right: 160px">
+
+            <table>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td style="text-align: justify">
+                        Sehubungan dengan pelaksanaan Magang/Praktek Kerja Lapangan di PT PLN (Persero) Kantor Pusat, atas nama :
+                    </td>
+                </tr>
+                <tr>
+                    <td>&emsp;</td>
+                    <td>&emsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table width="100%" border="1" align="center">
+                            <tr style="text-align: center">
+                                <th>No</th>
+                                <th>NIM</th>
+                                <th>Nama</th>
+                                <th>Jurusan</th>
+                            </tr>
+                            <tr>
+                                <td style="text-align: center"><?= $no++ ?></td>
+                                <td>&emsp; <?= $a['nim'] ?></td>
+                                <td>&emsp; <?= $a['nama_mhs']?></td>
+                                <td>&emsp; <?= $a['jurusan']?></td>
+                            </tr>
+                            <?php for($i=2;$i<=5;$i++) {
+                                if(!empty($a['nim_mhs'.$i])){?>
+                                    <tr>
+                                        <td style="text-align: center"><?= $i ?></td>
+                                        <td>&emsp; <?= $a['nim_mhs'.$i] ?></td>
+                                        <td>&emsp; <?= $a['nama_mhs'.$i]?></td>
+                                        <td>&emsp; <?= $a['jurusan']?></td>
+                                    </tr>
+                                <?php } ?>
+                            <?php }?>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&emsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align: justify">
+                        dengan ini kami sampaikan bahwa mahasiswa yang bersangkutan telah menyelesaikan magang di PT PLN (Persero) Kantor Pusat.
+                    </td>
+                </tr>
+                <tr>
+                    <td>&emsp;</td>
+                </tr>
+                <tr>
+                    <td>&emsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align: justify">
+                        Demikian disampaikan, atas perhatian dan kerjasamanya diucapkan terima kasih.
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <br><br>
+
+        <div style="margin-left: 200px">
+            <br><br>
+            <center>
+                <p>
+                    <b>VICE PRESIDENT HUMAN RESOURCES BUSSINESS</b><br>
+                    <b>PARTNER OF HEAD OFFICE</b>
+
+                    <br><br><br><br><br>
+                    <b>PRASETYORINI</b>
+                </p>
+            </center>
+        </div>
+    </div>
+<?php } ?>
